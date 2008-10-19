19d94661c8d384287dbcf2ae7388ce03dd05f096
Sjoerd Simons <sjoerd@luon.net>
2008-10-19 17:59:36 +0100

    Imported Upstream version 0.9.7

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# $Id: bootstrap.sh 6 2006-04-21 19:55:43Z lennart $
 
 # This file is part of pavucontrol.
 #
-# pavucontrol is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# Copyright 2006-2008 Lennart Poettering
+#
+# pavucontrol is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
 # pavucontrol is distributed in the hope that it will be useful, but
@@ -14,8 +15,7 @@
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pavucontrol; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+# along with pavucontrol. If not, see <http://www.gnu.org/licenses/>.
 
 VERSION=1.9
 
@@ -24,15 +24,15 @@ run_versioned() {
     local V
 
     V=$(echo "$2" | sed -e 's,\.,,g')
-    
-    if [ -e "`which $1$V`" ] ; then
-    	P="$1$V" 
+
+    if [ -e "`which $1$V 2> /dev/null`" ] ; then
+        P="$1$V"
     else
-	if [ -e "`which $1-$2`" ] ; then
-	    P="$1-$2" 
-	else
-	    P="$1"
-	fi
+        if [ -e "`which $1-$2 2> /dev/null`" ] ; then
+            P="$1-$2"
+        else
+            P="$1"
+        fi
     fi
 
     shift 2
@@ -44,16 +44,24 @@ set -ex
 if [ "x$1" = "xam" ] ; then
     run_versioned automake "$VERSION" -a -c --foreign
     ./config.status
-else 
+else
     rm -rf autom4te.cache
     rm -f config.cache
 
-    run_versioned aclocal "$VERSION"
+    rm -f Makefile.am~ configure.ac~
+    # Evil, evil, evil, evil hack
+    sed 's/read dummy/\#/' `which gettextize` | sh -s -- --copy --force
+    test -f Makefile.am~ && mv Makefile.am~ Makefile.am
+    test -f configure.ac~ && mv configure.ac~ configure.ac
+
+    intltoolize --copy --force --automake
+    run_versioned aclocal "$VERSION" -I m4
     run_versioned autoconf 2.59 -Wall
     run_versioned autoheader 2.59
     run_versioned automake "$VERSION" -a -c --foreign
 
-    CFLAGS="-g -O0" ./configure --sysconfdir=/etc "$@"
-
-    make clean
+    if test "x$NOCONFIGURE" = "x"; then
+        CFLAGS="-g -O0" ./configure --sysconfdir=/etc --localstatedir=/var "$@"
+        make clean
+    fi
 fi
